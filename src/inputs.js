1cd4f6984ec5d186cefcb9f19cecb84cba9041cb
sonnguyen <sonnguyen106mh@gmail.com>
2021-12-08 09:07:47 +0700

    learn about button

--- src/inputs.js
+++ src/inputs.js
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
-import {Text, View, StyleSheet, TextInput } from "react-native";
+import {Text, View, StyleSheet, TextInput, Button } from "react-native";
 const Inputs = () =>{
     const [myInput, setMyinput] = useState('');
+    const [names, setName] = useState(["John","James"])
     const onChangeInput = (event)=>{
         setMyinput(event)
     }
+    const onAddUser = ()=>{
+        const newState = [...names,myInput];
+        setName(newState);
+        setMyinput("");
+    }
     return (  
-        <TextInput
-            style = {styles.input}
-            value={myInput}
-            onChangeText = {(e)=>onChangeInput(e)}
-            multiline = {true}
-            maxLength = {25}
-            keyboardType = "number-pad"
-        />
+        <>
+            <TextInput
+                style = {styles.input}
+                value={myInput}
+                onChangeText = {(e)=>onChangeInput(e)}
+                multiline = {true}
+                maxLength = {25}
+                // keyboardType = "number-pad"
+                />
+            <Button title="Add user" onPress = {onAddUser}></Button>
+            {names.map(name=>(
+                <Text 
+                    style = {styles.user}
+                    key={name}
+                >{name}</Text>
+            ))}
+        </>  
     )
 }
 const styles = StyleSheet.create({
@@ -23,7 +38,13 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginTop: 20,
         padding: 10,
-
+    },
+    user: {
+        padding: 5,
+        fontSize: 20,
+        borderWidth: 1,
+        borderColor: '#cecece',
+        marginBottom: 5
     }
 })
 export default Inputs;
